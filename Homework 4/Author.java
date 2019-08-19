ae5a26b6de34c40087689a5e74c8253cf6c880db
NataliaDru <53942216+NataliaDru@users.noreply.github.com>
2019-08-20 00:18:46 +0700

    homework 4

---
+++ Homework 4/Author.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class Author {
+    String name;
+    String birthPlace;
+
+    public void setName(String _name) {
+        name = _name;
+    }
+
+    public void setBirthPlace(String _birthPlace) {
+        birthPlace = _birthPlace;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+}
